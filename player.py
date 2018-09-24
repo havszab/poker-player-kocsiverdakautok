8030d565f5b15bb34c39802cccc4f48b47dafda2
Dániel Takács <takacs.d24@gmail.com>
2018-09-24 16:11:07 +0200

    logic order modified

--- player.py
+++ player.py
@@ -2,18 +2,18 @@ import development
 
 
 class Player:
-    VERSION = "2.1.3 free round"
+    VERSION = "2.1.4 logic order modified "
 
     def betRequest(self, game_state):
         our_player = development.get_our_info(game_state)
-        if game_state["current_buy_in"] == int(game_state["small_blind"])*2:
-            return int(game_state["small_blind"])*2
         if development.is_card_in_hand_pair(our_player):
             return max(int(game_state["current_buy_in"]), int(our_player["stack"] * 0.25))
         if development.is_high_cards(our_player):
             if int(game_state["current_buy_in"]) > int(our_player["stack"]):
                 return int(our_player["stack"])
             return max(int(game_state["current_buy_in"]), int(our_player["stack"] * 0.1))
+        if game_state["current_buy_in"] == int(game_state["small_blind"])*2:
+            return int(game_state["small_blind"])*2
         return 0
 
     def showdown(self, game_state):
