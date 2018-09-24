ac63ea8e53fc9520a39654f20c722cb79177b24f
havszab <havaldaszab@gmail.com>
2018-09-24 13:48:43 +0200

    pair check in hand

--- development.py
+++ development.py
@@ -16,4 +16,12 @@ def is_high_cards(our_player):
         return False
 
 
+def is_card_in_hand_pair(our_player):
+    hand = our_player["hole_cards"]
+    if hand[0]["rank"] == hand[1]["rank"]:
+        return True
+    return False
+
+
+
 print(test["game_state"]["current_buy_in"])
