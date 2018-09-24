72bb8bffea6a5f507cf61821f1fb22bc32dd9afd
TribTrob <trib073@gmail.com>
2018-09-24 13:37:19 +0200

    development

---
+++ development.py
@@ -0,0 +1,19 @@
+test ={"game_state":{"tournament_id":"5b968a7bb7b7260004000002","game_id":"5ba8b9933e762d0004000078","round":0,"players":[{"name":"Legnehezebb","stack":1000,"status":"active","bet":0,"hole_cards":[],"time_used":0,"version":"0.4.3","id":0},{"name":"team Korda","stack":1000,"status":"active","bet":0,"hole_cards":[],"time_used":0,"version":"1.0 Black Widow","id":1},{"name":"Eager Ducks","stack":1000,"status":"active","bet":0,"hole_cards":[],"time_used":0,"version":"Default Java folding player","id":2},{"name":"The Embarrassed Partridges","stack":1000,"status":"active","bet":0,"hole_cards":[],"time_used":0,"version":"Java player V0.1","id":3},{"name":"pokerInterFace","stack":1000,"status":"active","bet":0,"hole_cards":[],"time_used":0,"version":"0.1","id":4},{"name":"kocsiverdakautok","stack":1000,"status":"active","bet":0,"hole_cards":["A", "A"],"time_used":0,"version":"1.2","id":5}],"small_blind":2,"big_blind":4,"orbits":0,"dealer":4,"community_cards":[],"current_buy_in":0,"pot":0}}
+
+
+def get_our_info(game_state):
+    for player in game_state["game_state"]["players"]:
+        if player["name"] is "kocsiverdakautok":
+            return player
+
+
+def is_high_cards(our_player):
+    hand = our_player["hole_cards"]
+    high_card = ["A", "K", "Q", "J"]
+    if hand[0] in high_card or hand[1] in high_card:
+        return True
+    else:
+        return False
+
+
+print(test["game_state"]["current_buy_in"])
--- player.py
+++ player.py
@@ -1,9 +1,14 @@
+import development
+
 
 class Player:
-    VERSION = "1.2"
+    VERSION = "1.3"
 
     def betRequest(self, game_state):
-        return int(game_state["small_blind"])*2
+        our_player = development.get_our_info()
+        if development.is_high_cards(our_player):
+            return max(game_state["game_state"]["current_buy_in"], our_player["stack"]*0.1)
+        return 0
 
     def showdown(self, game_state):
         pass
