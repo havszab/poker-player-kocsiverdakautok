3d22820e26d1ffa5fdc561e725ad575da15b6a6d
havszab <havaldaszab@gmail.com>
2018-09-24 15:15:42 +0200

    bugfix

--- player.py
+++ player.py
@@ -1,13 +1,16 @@
+import development
+
 
 class Player:
-    VERSION = "2.2 Never Lucky"
-
+    VERSION = "2.1.2 new hope"
 
     def betRequest(self, game_state):
-        for player in game_state["player"]:
-            if player["name"] == "kocsiverdakautok":
-                return player
-
+        our_player = development.get_our_info(game_state)
+        if development.is_high_cards(our_player):
+            if int(game_state["current_buy_in"]) > int(our_player["stack"]):
+                return int(our_player["stack"])
+            return max(int(game_state["current_buy_in"]), int(our_player["stack"] * 0.1))
+        return 0
 
     def showdown(self, game_state):
         pass
