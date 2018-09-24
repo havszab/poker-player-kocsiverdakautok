c41ddec94791e827220f9539184db06da171729c
havszab <havaldaszab@gmail.com>
2018-09-24 14:34:12 +0200

    in race again

--- development.py
+++ development.py
@@ -23,10 +23,5 @@ def is_card_in_hand_pair(our_player):
     return False
 """
     def betRequest(self, game_state):
-        our_player = development.get_our_info()
-        if development.is_high_cards(our_player):
-            if int(game_state["current_buy_in"]) > int(our_player["stack"]):
-                return int(our_player["stack"])
-            return max(int(game_state["current_buy_in"]), int(our_player["stack"]*0.1))
-        return 0
+
 """
--- player.py
+++ player.py
@@ -2,11 +2,16 @@ import development
 
 
 class Player:
-    VERSION = "1.4"
+    VERSION = "2.0"
 
 
     def betRequest(self, game_state):
-        return 900
+        our_player = development.get_our_info(game_state)
+        if development.is_high_cards(our_player):
+            if int(game_state["current_buy_in"]) > int(our_player["stack"]):
+                return int(our_player["stack"])
+            return max(int(game_state["current_buy_in"]), int(our_player["stack"]*0.1))
+        return 0
 
     def showdown(self, game_state):
         pass
